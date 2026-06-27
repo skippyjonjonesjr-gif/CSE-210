7b7d8fa936cedddfa19bff2c0496f601b9d8beca
skippyjonjonesjr-gif <skippyjonjonesjr@gmail.com>
2026-06-27 16:37:36 -0600

    Add files via upload

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4.0));
+        shapes.Add(new Rectangle("Blue", 5.0, 6.0));
+        shapes.Add(new Circle("Green", 3.0));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {shape.GetType().Name} has a color of {color} and an area of {area:F2}.");
+        }
     }
 }
